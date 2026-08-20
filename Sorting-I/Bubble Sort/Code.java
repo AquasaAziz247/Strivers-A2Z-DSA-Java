a159b8fe9465ae6223b0e6f5a77ac727bd1c96cb
Aquasa Aziz <119380157+AquasaAziz247@users.noreply.github.com>
2026-08-21 00:55:35 +0530

    Implement bubble sort algorithm in Code.java

---
+++ Sorting-I/Bubble Sort/Code.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    static void bubbleSort(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 4, 3, 2, 1};
+
+        bubbleSort(arr);
+
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
+
