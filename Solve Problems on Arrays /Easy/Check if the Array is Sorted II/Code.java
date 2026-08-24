af1d1ac978a21a6fe7335dcac2b8a4f618473889
Aquasa Aziz <119380157+AquasaAziz247@users.noreply.github.com>
2026-08-24 12:58:42 +0530

    Add solution to check if array is sorted II
    Implement a solution to check if an array is sorted with at most one rotation.

---
+++ Solve Problems on Arrays /Easy/Check if the Array is Sorted II/Code.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int drops=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>nums[(i+1)%nums.length]){
+                drops++;
+            }
+            if(drops>1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+TC : 0(N)
+SC :0(1)
