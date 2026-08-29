e95be88a7d1731a975dcf2c7742f5173a40f526e
Aquasa Aziz <119380157+AquasaAziz247@users.noreply.github.com>
2026-08-29 22:38:45 +0530

    Implement solution to find single number in array

---
+++ Solve Problems on Arrays /Easy/Find the number that appears once, and other numbers twice./Code.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int num : nums){
+            hm.put(num,hm.getOrDefault(num,0)+1);
+        }
+        for(int num:hm.keySet()){
+            if(hm.get(num)==1){
+                return num;
+            }
+
+        }
+        return -1;
+    }
+}
