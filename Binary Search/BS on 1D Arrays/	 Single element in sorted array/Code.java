e7b93378503dabfcd4490078c0aa04640fb814ce
Aquasa Aziz <119380157+AquasaAziz247@users.noreply.github.com>
2026-09-21 23:28:28 +0530

    Create Code.java

---
+++ Binary Search/BS on 1D Arrays/	 Single element in sorted array/Code.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        
+        int n = nums.length;
+        
+        // If only one element
+        if (n == 1) {
+            return nums[0];
+        }
+        
+        // Check boundary elements
+        if (nums[0] != nums[1]) {
+            return nums[0];
+        }
+        
+        if (nums[n - 1] != nums[n - 2]) {
+            return nums[n - 1];
+        }
+        
+        int low = 1;
+        int high = n - 2;
+        
+        while (low <= high) {
+            
+            int mid = low + (high - low) / 2;
+            
+            // Found the single element
+            if (nums[mid] != nums[mid - 1] && 
+                nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
+            
+            // We are in the left half
+            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) ||
+                (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                
+                low = mid + 1;
+            }
+            
+            // We are in the right half
+            else {
+                high = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+}
